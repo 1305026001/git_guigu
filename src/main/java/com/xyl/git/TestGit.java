12a3365a633d2a3c105b68c9d83d21ce2726a741
xyl <1305026001@qq.com>
2021-06-17 20:46:06 +0800

    first commit to git

--- src/main/java/com/xyl/git/TestGit.java
+++ src/main/java/com/xyl/git/TestGit.java
@@ -1,10 +1,14 @@
 package com.xyl.git;
 
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 public class TestGit {
     public static void main(String[] args) {
         ZonedDateTime now = ZonedDateTime.now();
         System.out.println("hello git,当地的现在时间是 : " + now);
+
+        String str = UUID.randomUUID().toString();
+        System.out.println(str);
     }
 }
