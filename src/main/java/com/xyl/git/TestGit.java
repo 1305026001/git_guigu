de027f809bff76e1124cff1f301bcdd15565f4d9
xyl <1305026001@qq.com>
2021-06-17 20:50:53 +0800

    third commit to git

--- src/main/java/com/xyl/git/TestGit.java
+++ src/main/java/com/xyl/git/TestGit.java
@@ -6,9 +6,12 @@ import java.util.UUID;
 public class TestGit {
     public static void main(String[] args) {
         ZonedDateTime now = ZonedDateTime.now();
-        System.out.println("hello git,当地的现在时间是 : " + now);
+        System.out.println("hello git1,当地的现在时间是 : " + now);
 
         String str = UUID.randomUUID().toString();
-        System.out.println(str);
+        System.out.println("hello git2,"+str);
+
+
+        System.out.println("hello git3,"+Runtime.getRuntime().availableProcessors());
     }
 }
